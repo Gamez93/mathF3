a708e773f88f04aa50d5dfedaec79ab77087a773
Gamez93 <nls.gmz.93@gmail.com>
2020-04-10 22:29:58 -0600

    inicio
    Co-Authored-By: null <Gamez93@users.noreply.github.com>

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <h1>Example heading <span class="badge badge-secondary">New</span></h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
