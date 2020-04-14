585d501bba445fb7e8029d6c8f7b4485aff6519d
Gamez93 <nls.gmz.93@gmail.com>
2020-04-14 00:34:41 -0600

    template inicial

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,62 @@
 @extends('layouts.app')
 
+@section('sidebar')
+<div class="container">
+          <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+              <div class="panel panel-default">
+                  <div class="panel-heading" role="tab" id="headingOne">
+                      <h4 class="panel-title">
+                          <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                              Clases
+                          </a>
+                      </h4>
+                  </div>
+                  <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+                      <div class="panel-body">
+                          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nisl lorem, dictum id pellentesque at, vestibulum ut arcu. Curabitur erat libero, egestas eu tincidunt ac, rutrum ac justo. Vivamus condimentum laoreet lectus, blandit posuere tortor aliquam vitae. Curabitur molestie eros. </p>
+                      </div>
+                  </div>
+              </div>
+
+              <div class="panel panel-default">
+                  <div class="panel-heading" role="tab" id="headingTwo">
+                      <h4 class="panel-title">
+                          <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                              Multimedia
+                          </a>
+                      </h4>
+                  </div>
+                  <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+                      <div class="panel-body">
+                          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nisl lorem, dictum id pellentesque at, vestibulum ut arcu. Curabitur erat libero, egestas eu tincidunt ac, rutrum ac justo. Vivamus condimentum laoreet lectus, blandit posuere tortor aliquam vitae. Curabitur molestie eros. </p>
+                      </div>
+                  </div>
+              </div>
+
+              <div class="panel panel-default">
+                  <div class="panel-heading" role="tab" id="headingThree">
+                      <h4 class="panel-title">
+                          <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                              Opciones
+                          </a>
+                      </h4>
+                  </div>
+                  <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+                      <div class="panel-body">
+                          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nisl lorem, dictum id pellentesque at, vestibulum ut arcu. Curabitur erat libero, egestas eu tincidunt ac, rutrum ac justo. Vivamus condimentum laoreet lectus, blandit posuere tortor aliquam vitae. Curabitur molestie eros. </p>
+                      </div>
+                  </div>
+              </div>
+          </div>
+        </div>
+@endsection
+
+
 @section('content')
 <div class="container">
+  <button type="button" class="btn btn-secondary btn-lg btn-block">Hoja de Estudio</button>
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-10">
             <div class="card">
                 <div class="card-header">Dashboard</div>
 
@@ -13,11 +66,13 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    <h1>Example heading <span class="badge badge-secondary">New</span></h1>
                 </div>
             </div>
         </div>
     </div>
 </div>
+@endsection
+
+@section('graf')
+  <button type="button" class="btn btn-primary btn-lg btn-block">Graficas</button>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,25 +20,28 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
       -->
 
-<!-- jquery
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
--->
-<script src="{{ asset('bootstrap/js/jquery.min.js') }}" defer></script>
-<script src="{{ asset('bootstrap/js/popper.min.js') }}" defer></script>
+    <!-- jquery
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    -->
+    <script src="{{ asset('bootstrap/js/jquery.min.js') }}" defer></script>
+    <script src="{{ asset('bootstrap/js/popper.min.js') }}" defer></script>
 
-<!-- bootstrap-->
-<link href="{{ asset('bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-<script src="{{ asset('bootstrap/js/bootstrap.min.js') }}" defer></script>
+    <!-- bootstrap-->
+    <link href="{{ asset('bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('bootstrap/css/sidebar.css') }}" rel="stylesheet">
+    <script src="{{ asset('bootstrap/js/bootstrap.min.js') }}" defer></script>
 
-<!--
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
--->
+    <!--
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    -->
+
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+  <div class="">
+    <div class="row">
+      <div class="col-sm"><nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     Herramienta Matem&aacute;tica
@@ -87,12 +90,29 @@
                     </ul>
                 </div>
             </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
+        </nav></div>
     </div>
+    <div class="row">
+      <div class="col-sm">
+        <button type="button" class="btn btn-secondary btn-lg btn-block">Barra de Opciones</button>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm">
+        <br>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-2">@yield('sidebar')</div>
+      <div class="col-sm-6">@yield('content')</div>
+      <div class="col-sm-4">@yield('graf')</div>
+    </div>
+    <div class="row">
+      <div class="col-sm">
+        <button type="button" class="btn btn-secondary btn-lg btn-block">Opcional</button>
+      </div>
+    </div>
+</div>
 
 </body>
 </html>
