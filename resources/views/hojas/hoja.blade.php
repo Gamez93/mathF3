41c887e721818d5deada0927f990ab0b26b9f562
Gamez93 <nls.gmz.93@gmail.com>
2020-04-19 22:52:17 -0600

    opciones de hoja
    Co-Authored-By: Nelson Gamez <nls.gmz.93@gmail.com>

--- resources/views/hojas/hoja.blade.php
+++ resources/views/hojas/hoja.blade.php
@@ -10,12 +10,44 @@
 
 @section('content')
 <div class="container">
+  <!--Encabezado de la hoja donde interactua el usuario -->
   <button type="button" class="btn btn-secondary btn-lg btn-block">
     <img src="../public/icons/file-text.svg" alt="" width="25" height="25" title="Hoja">
     Hoja de Estudio
   </button>
 
-</div>
+  <!--Opciones de la Hoja -->
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-item nav-link" href="#">
+          <img src="../public/icons/file-earmark-plus.svg" alt="" width="25" height="25" title="Nuevo">
+        </a>
+        <a class="nav-item nav-link" href="#">
+          <img src="../public/icons/upload.svg" alt="" width="25" height="25" title="Cargar">
+        </a>
+        <a class="nav-item nav-link" href="#">
+          <img src="../public/icons/download.svg" alt="" width="25" height="25" title="Descargar">
+        </a>
+        <a class="nav-item nav-link" href="#">
+          <img src="../public/icons/graph-up.svg" alt="" width="25" height="25" title="Graficar">
+        </a>
+        <a class="nav-item nav-link" href="#">
+          <img src="../public/icons/play.svg" alt="" width="25" height="25" title="Resolver">
+        </a>
+        <a class="nav-item nav-link" href="#">
+          <img src="../public/icons/plus-square.svg" alt="" width="25" height="25" title="Guardar">
+        </a>
+        <a class="nav-item nav-link" href="#">
+          <img src="../public/icons/trash.svg" alt="" width="25" height="25" title="Borrar">
+        </a>
+        <a class="nav-item nav-link" href="#">
+          <img src="../public/icons/question-square.svg" alt="" width="25" height="25" title="Ayuda">
+        </a>
+      </div>
+    </div>
+  </nav>
+</div><!-- cierre de seccion content-->
 @endsection
 
 @section('graf')
